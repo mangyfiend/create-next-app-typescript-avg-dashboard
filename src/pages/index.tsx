e60722f9eebefb15d7cce6dbf256350708f3674d
monolithgoon <svanteahrrenus@hotmail.com>
2023-01-09 08:51:46 +0100

    Added function type to getServerSideProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { GetServerSideProps } from "next";
 import Link from "next/link";
 import Layout from "@components/projects/avg-dashboard/Layouts/Layout";
 import API_URLS from "@utils/constants/api-urls";
@@ -5,11 +6,13 @@ import Dashboard from "@components/projects/avg-dashboard/Dashboard-v4";
 import { DashboardProvider } from "@context/projects/avg-dashboard/DashboardContext";
 import IGeoclusterGeoJSON from "@interfaces/projects/avg-dashboard/IGeoclusterGeoJSON";
 
+
 // TODO > CHANGE TO IMPORT
 interface IGeoclustersAPIData {
 	collection_docs: IGeoclusterGeoJSON[];
 	collection_docs_num: number;
 }
+
 
 // TODO > CHANGE TO IMPORT
 interface IGeoclusterAPIResponse {
@@ -17,7 +20,8 @@ interface IGeoclusterAPIResponse {
 	data: IGeoclustersAPIData;
 }
 
-export default function AVGDashboard(props: { cachedGeoclustersArray: IGeoclusterGeoJSON[] }) {
+
+export default function AVGDashboard(props: { cachedGeoclusters: IGeoclusterGeoJSON[] }) {
 	return (
 		<Layout
 			title="AVG Dashboard"
@@ -25,7 +29,7 @@ export default function AVGDashboard(props: { cachedGeoclustersArray: IGeocluste
 			keywords="AGC, NIRSAL, Dashboard, React, Next.js">
 			<p>Hello Next.js & Typescript👋</p>
 			<DashboardProvider>
-				<Dashboard cachedClustersArray={props.cachedGeoclustersArray}></Dashboard>
+				<Dashboard cachedClustersArray={props.cachedGeoclusters}></Dashboard>
 			</DashboardProvider>
 			<p>
 				<Link href="/about">About</Link>
@@ -34,13 +38,18 @@ export default function AVGDashboard(props: { cachedGeoclustersArray: IGeocluste
 	);
 }
 
-export async function getServerSideProps() {
+
+// MTD. 1 > using fully typed named function with GetServerSideProps type
+export const getServerSideProps: GetServerSideProps = async (): Promise<
+	| { props: { cachedGeoclusters: IGeoclusterGeoJSON[]; admin1Bounds: any; admin2Bounds: any } }
+	| { props: { cachedGeoclusters: any; admin1Bounds?: undefined; admin2Bounds?: undefined } }
+> => {
 	try {
 		let apiResponse = await fetch(API_URLS.AWSEC2.PARCELIZED_GEOCLUSTERS);
 		const apiData: IGeoclusterAPIResponse = await apiResponse.json();
 		return {
 			props: {
-				cachedGeoclustersArray: apiData.data.collection_docs,
+				cachedGeoclusters: apiData.data.collection_docs,
 				admin1Bounds: null,
 				admin2Bounds: null,
 			},
@@ -50,4 +59,27 @@ export async function getServerSideProps() {
 			props: { cachedGeoclusters: null },
 		};
 	}
-}
+};
+
+
+// MTD. 2 > GetServerSideProps type omitted
+// export async function getServerSideProps(): Promise<
+// 	| { props: { cachedGeoclusters: IGeoclusterGeoJSON[]; admin1Bounds: any; admin2Bounds: any } }
+// 	| { props: { cachedGeoclusters: any; admin1Bounds?: undefined; admin2Bounds?: undefined } }
+// > {
+// 	try {
+// 		let apiResponse = await fetch(API_URLS.AWSEC2.PARCELIZED_GEOCLUSTERS);
+// 		const apiData: IGeoclusterAPIResponse = await apiResponse.json();
+// 		return {
+// 			props: {
+// 				cachedGeoclusters: apiData.data.collection_docs,
+// 				admin1Bounds: null,
+// 				admin2Bounds: null,
+// 			},
+// 		};
+// 	} catch (err) {
+// 		return {
+// 			props: { cachedGeoclusters: null },
+// 		};
+// 	}
+// }
